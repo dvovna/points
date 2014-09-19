1e101922d6954d428651f21321b7727d55025b13
dvovna <d.vovna.dv@gmail.com>
2014-09-19 17:06:50 +0300

    impl;

--- points.js
+++ points.js
@@ -1,3 +1,19 @@
-(function () {
+var svg = d3.select('body').append('svg')
 
-}());
+svg
+    .attr('x', 50)
+    .attr('y', 50);
+
+svg
+    .append('circle')
+    .attr('cx', 200)
+    .attr('cy', 200)
+    .attr('r', 10)
+    .attr('fill', 'yellow')
+    .transition()
+    .duration(1000)
+    .attr('fill', 'red')
+    .transition()
+    .duration(1000)
+    .attr('fill', 'yellow')
+
