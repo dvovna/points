3af20eef1f3798c97ba9f7fd00c1e2eee8409d69
dvovna <d.vovna.dv@gmail.com>
2014-09-19 17:29:33 +0300

    impl;

--- points.js
+++ points.js
@@ -1,19 +1,32 @@
-var svg = d3.select('body').append('svg')
+(function () {
+    var svg = d3.select('body').append('svg');
 
-svg
-    .attr('x', 50)
-    .attr('y', 50);
+    svg
+        .attr('x', 50)
+        .attr('y', 50)
+        .append('circle')
+        .attr('cx', 200)
+        .attr('cy', 200)
+        .attr('r', 10);
 
-svg
-    .append('circle')
-    .attr('cx', 200)
-    .attr('cy', 200)
-    .attr('r', 10)
-    .attr('fill', 'yellow')
-    .transition()
-    .duration(1000)
-    .attr('fill', 'red')
-    .transition()
-    .duration(1000)
-    .attr('fill', 'yellow')
+    svg
+        .selectAll('circle')
+        .attr('fill', 'red');
+
+
+    function startColorAnimation(color) {
+        var colorToSet = color === "red" ? "yellow" : "red";
+
+        svg
+            .selectAll('circle')
+            .transition()
+            .duration(1000)
+            .attr('fill', colorToSet)
+            .each("end", function () {
+                startColorAnimation(colorToSet);
+            });
+    }
+
+    startColorAnimation('red');
+}());
 
