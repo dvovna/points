506dd9b7b9bbbb40211b36d0910dbe7be1d5eabc
dvovna <d.vovna.dv@gmail.com>
2014-09-22 17:11:24 +0300

    one ball impl;

--- points.js
+++ points.js
@@ -1,32 +1,87 @@
 (function () {
-    var svg = d3.select('body').append('svg');
+    var svg = d3.select('body').append('svg'),
+        circle = svg.append('circle'),
+        svgHeight = parseInt(svg.style("height"), 10),
+        svgWidth = parseInt(svg.style('width'), 10),
+        circleSize = 10,
+        vector = {
+            x: 1,
+            y: 1
+        };
 
-    svg
-        .attr('x', 50)
-        .attr('y', 50)
-        .append('circle')
-        .attr('cx', 200)
-        .attr('cy', 200)
-        .attr('r', 10);
-
-    svg
-        .selectAll('circle')
+    circle
+        .attr('cx', 10)
+        .attr('cy', 10)
+        .attr('r', circleSize)
         .attr('fill', 'red');
 
-
-    function startColorAnimation(color) {
-        var colorToSet = color === "red" ? "yellow" : "red";
-
-        svg
-            .selectAll('circle')
-            .transition()
-            .duration(1000)
-            .attr('fill', colorToSet)
-            .each("end", function () {
-                startColorAnimation(colorToSet);
-            });
+    function checkForXEnd(currX) {
+        if (vector.x > 0 && (currX + circleSize) === svgWidth) {
+            vector.x = -1;
+            return;
+        } else if (vector.x < 0 && (currX - circleSize) === 0) {
+            vector.x = 1;
+            return;
+        }
     }
 
-    startColorAnimation('red');
+    function checkForYEnd(currY) {
+        if (vector.y > 0 && (currY + circleSize) === svgHeight) {
+            vector.y = -1;
+            return;
+        } else if (vector.y < 0 && (currY - circleSize) === 0) {
+            vector.y = 1;
+            return;
+        }
+    }
+
+    function getX() {
+        var x = parseInt((this.attributes.cx.value), 10);
+
+        checkForXEnd(x);
+
+        if (vector.x > 0) {
+            x++;
+        } else {
+            x--;
+        }
+        return x;
+    }
+
+    function getY() {
+        var y = parseInt((this.attributes.cy.value), 10);
+
+        checkForYEnd(y);
+
+        if (vector.y > 0) {
+            y++;
+        } else {
+            y--;
+        }
+
+        return y;
+    }
+
+    function start(color, speed) {
+        var colorToSet = color === "red" ? "yellow" : "red";
+
+        circle
+            .transition()
+            .delay(500)
+            .duration(speed)
+            .attr('fill', colorToSet)
+            .each("end", function () {
+                start(colorToSet, speed);
+            });
+
+        setInterval(function () {
+            circle
+                .attr('cx', getX)
+                .attr('cy', getY)
+        }, 50);
+}
+
+    start('red', 5000);
+
 }());
 
