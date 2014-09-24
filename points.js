32b236b66842a69b6db34d576dbac64394eeca71
dvovna <d.vovna.dv@gmail.com>
2014-09-24 11:22:39 +0300

    form added;

--- points.js
+++ points.js
@@ -8,14 +8,23 @@
         cy,
         radius;
 
-    for (var i = 1; i <= 300; i = i + 1) {
-        radius = Math.floor(Math.random() * 50 + 1);
-        cx = i * Math.floor(Math.random() * 10 + 1);
-        cy = i * Math.floor(Math.random() * 10 + 1);
-        colorAnimationTime = radius * 10 + 500;
-        moveAnimationTime = radius;
+    function createPoints(isPointOrCircle) {
+        svg.selectAll("circle").remove();
 
-        Point(svgWidth, svgHeight, radius, cx, cy, colorAnimationTime, moveAnimationTime);
+        for (var i = 1; i <= 100; i = i + 1) {
+            radius = !isPointOrCircle ? Math.floor(Math.random() * 50 + 1) : 2;
+            cx = i * Math.floor(Math.random() * 10 + 1);
+            cy = i * Math.floor(Math.random() * 10 + 1);
+            colorAnimationTime = radius * 10 + 500;
+            moveAnimationTime = radius;
+
+            Point(svgWidth, svgHeight, radius, cx, cy, colorAnimationTime, moveAnimationTime);
+        }
     }
+
+    document.getElementById("is_point").onchange = function () {
+        createPoints(this.checked);
+    };
+    createPoints(false);
 }());
 
